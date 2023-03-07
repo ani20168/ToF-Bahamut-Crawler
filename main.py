baa3d25eea91aed5e67688da3fdc2890199f6864
ani20168 <50546458+ani20168@users.noreply.github.com>
2023-03-07 10:37:53 +0800

    重構

--- main.py
+++ main.py
@@ -1,91 +1,88 @@
 import requests
 from bs4 import BeautifulSoup
-import ctypes
 import re
-import time,datetime
+import time
+import datetime
 import os
-from playsound import playsound
 import threading
+from playsound import playsound
 import webhook
 
-#篩選列表
-filters = ["虛寶", "序號", "禮包"]
-filters_ignore = ["序號分享及兌換方式"]
-#是否使用 Discord 通知? 如需使用，請在下方新增你的webhook網址
-webhook.url = ""
+# 定義常數
+URL = "https://forum.gamer.com.tw/B.php?bsn=71040" #要爬取的哈拉版
+FILTERS = ["虛寶", "序號", "禮包"]                  #篩選這些標題
+FILTERS_IGNORE = ["序號分享及兌換方式"]             #忽略標題
+webhook.url = ""                                   #是否使用 Discord 通知? 如需使用，請在這裡新增你的webhook網址
+SOUND_FILE_PATH = os.path.join(os.getcwd(), "sound.wav")
 
-#提示音
-def play_sound():
-    current_dir = os.getcwd()
-    file_path = os.path.join(current_dir, 'sound.wav')
-    if os.path.exists(file_path):
-        playsound(file_path)
-
-#表頭
-headers = {
+# 定義表頭
+HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
 }
 
-# 顯示過的文章
-previous_articles = []
+# 定義已搜索過的文章列表
+SEARCHED_ARTICLES = []
 
-print('''
-====================
- 巴哈哈拉版序號爬蟲
-====================''')
+# 定義提示音函數
+def play_sound():
+    if os.path.exists(SOUND_FILE_PATH):
+        playsound(SOUND_FILE_PATH)
 
-while True:
-    # 使用 requests 套件請求目標網站的 HTML 頁面
-    response = requests.get("https://forum.gamer.com.tw/B.php?bsn=71040",headers=headers)
-
-    # 將 HTML 頁面轉換成 BeautifulSoup 物件
+# 定義爬蟲函數
+def crawl():
+    response = requests.get(URL, headers=HEADERS)
     soup = BeautifulSoup(response.text, "html.parser")
-    # 尋找文章元素
-    elements = soup.find_all(class_="b-list__row b-list-item b-imglist-item")
+    article_elements = soup.find_all(class_="b-list__row b-list-item b-imglist-item")
+
     new_articles = []
-    for element in elements:
-        #尋找文章標題
-        title_element = element.find(class_="b-list__main__title")
-        #篩選標題
-        if any(filter in title_element.text for filter in filters):
-            if not any(filter in title_element.text for filter in filters_ignore):
-                # 檢查當前文章是否曾被搜索過
-                if title_element.text not in previous_articles:
-                    # 印出新文章的標題
-                    ctypes.windll.kernel32.SetConsoleTextAttribute(ctypes.windll.kernel32.GetStdHandle(-11), 2)
-                    print("【標題:{}】".format(title_element.text))
-                    ctypes.windll.kernel32.SetConsoleTextAttribute(ctypes.windll.kernel32.GetStdHandle(-11), 7)
-                    webhook.ContentAdd(title_element.text) if webhook.url else None
+    for article in article_elements:
+        title_element = article.find(class_="b-list__main__title")
+        title = title_element.text
 
-                    #尋找內文網址
-                    article_url = title_element.get("href")
-                    article_url = "https://forum.gamer.com.tw/" + article_url
+        # 過濾文章
+        if any(filter in title for filter in FILTERS) and not any(filter in title for filter in FILTERS_IGNORE):
+            if title not in SEARCHED_ARTICLES:
+                # 新文章，進行處理
+                print(f"【標題：{title}】")
+                webhook.ContentAdd(title) if webhook.url else None
 
-                    response = requests.get(article_url,headers=headers)
-                    article_soup = BeautifulSoup(response.text, "html.parser")
-                    content_elements = article_soup.find_all(class_="c-article__content")
-                    for content_element in content_elements:
-                        # 序號規則匹配
-                        pattern = r'[A-Za-z0-9]{13}'
-                        # 提取序號並顯示
-                        codes = re.findall(pattern, content_element.get_text())
-                        for code in codes:
-                            print(code)
-                            webhook.ContentAdd(code) if webhook.url else None
+                article_url = "https://forum.gamer.com.tw/" + title_element.get("href")
+                response = requests.get(article_url, headers=HEADERS)
+                article_soup = BeautifulSoup(response.text, "html.parser")
+                content_elements = article_soup.find_all(class_="c-article__content")
 
-                    # 當前文章加入到搜索歷史
-                    previous_articles.append(title_element.text)
-                    new_articles.append(title_element.text)
-    
+                codes = []
+                for content_element in content_elements:
+                    # 匹配序號
+                    pattern = r"[A-Za-z0-9]{13}"
+                    codes.extend(re.findall(pattern, content_element.get_text()))
+
+                # 輸出序號
+                for code in codes:
+                    print(code)
+                    webhook.ContentAdd(code) if webhook.url else None
+
+                # 將已搜索過的文章加入列表
+                SEARCHED_ARTICLES.append(title)
+                new_articles.append(title)
+
     if len(new_articles) > 0:
-        now = datetime.datetime.now()
-        time_str = now.strftime("%m/%d %H:%M")
-        print("{} 找到了 {} 篇新文章".format(time_str, len(new_articles)))
-        thread = threading.Thread(target=play_sound)
-        thread.start()
+        # 有新文章，提示並發送 Discord 通知
+        now = datetime.datetime.now().strftime("%m/%d %H:%M")
+        print(f"{now} 找到了 {len(new_articles)} 篇新文章")
+        threading.Thread(target=play_sound).start()
         webhook.Post() if webhook.url else None
 
-    time.sleep(10)
+# 主程式
+if __name__ == "__main__":
+    print("============")
+    print("巴哈哈拉版序號爬蟲")
+    print("============")
+
+    while True:
+        crawl()
+        time.sleep(10)
+
 
 
 
